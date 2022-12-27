60296647f6c3d7c013429505ffa7942b64794d46
Cristian Rivera Acevedo <cristian.rivera3284@gmail.com>
2022-12-27 02:58:40 +0000

    Piedra, papel o tijeras con ciclo While

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
-let jugadorA = prompt("Jugador A");
-let jugadorB = prompt("Jugador B");
+
+let iniciar = prompt ("¿Iniciar?");
+let contadorA = 0;
+let contadorB = 0;
+
+//inicia ciclo while
+
+while (iniciar == "si")
+{
+   
+   let jugadorA = prompt("Jugador A");
+   let jugadorB = prompt("Jugador B");
 
 if(jugadorA == "piedra" && jugadorB == "tijeras"){
-    document.body.innerHTML = "Gana jugador A"
+  contadorA = contadorA + 1;
  } else if (jugadorA == "piedra" && jugadorB == "papel"){
-    document.body.innerHTML = "Gana jugador B"
+   contadorB = contadorB + 1;
  } else if (jugadorA == "tijeras" && jugadorB == "piedra"){
-    document.body.innerHTML = "Gana jugador B"
+   contadorB = contadorB + 1;
  } else if (jugadorA == "tijeras" && jugadorB == "papel"){
-    document.body.innerHTML = "Gana jugador A"
+   contadorA = contadorA + 1;
  } else if (jugadorA == "papel" && jugadorB == "tijeras") {
-    document.body.innerHTML = "Gana jugador B"
+   contadorB = contadorB + 1;
  } else if (jugadorA == "papel" && jugadorB == "piedra") {
-    document.body.innerHTML = "Gana jugador A"
+   contadorA = contadorA + 1;
  } else if (jugadorA == jugadorB) {
     document.body.innerHTML = "Empate";
  } else {
     document.body.innerHTML = "Valores erróneos"
  }
+
+ iniciar = prompt("Quiere iniciar otro juego (si/no)");
+}
+
+if (iniciar == "no"){
+   document.body.innerHTML = "las victorias del jugador A son: " + contadorA + "<br>las victorias del jugador B son: " + contadorB;
+}
+
 
  /* hacer contador de número de partidas y victorias de cada jugador
   hacer el print en html y no en consola document.body.innerHTML */
