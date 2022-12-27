3b4595675bfe011c3f264815bd584f3aa4331892
Cristian Rivera Acevedo <cristian.rivera3284@gmail.com>
2022-12-27 06:20:15 +0000

    Actualización de alertas

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 //variables
-let iniciar = prompt ("¿Iniciar el juego? (si/no)");
+
 let contadorA = 0;
 let contadorB = 0;
+
+if (confirm("¿Iniciar juego?")) {
+  iniciar = "si";
+} else {
+  iniciar = "no";
+}
 
 //inicia ciclo while
 
 while (iniciar == "si")
 {
    
-   let jugadorA = prompt("Jugador A");
-   let jugadorB = prompt("Jugador B");
+   let jugadorA = prompt("Jugador A" , "piedra, papel o tijera");
+   let jugadorB = prompt("Jugador B" , "piedra, papel o tijera");
 
-      if(jugadorA == "piedra" && jugadorB == "tijeras"){
-      contadorA = contadorA + 1;
+      if(jugadorA == "piedra" && jugadorB == "tijera"){
+      contadorA++;
       } else if (jugadorA == "piedra" && jugadorB == "papel"){
-         contadorB = contadorB + 1;
-      } else if (jugadorA == "tijeras" && jugadorB == "piedra"){
-         contadorB = contadorB + 1;
-      } else if (jugadorA == "tijeras" && jugadorB == "papel"){
-         contadorA = contadorA + 1;
-      } else if (jugadorA == "papel" && jugadorB == "tijeras") {
-         contadorB = contadorB + 1;
+         contadorB++;
+      } else if (jugadorA == "tijera" && jugadorB == "piedra"){
+         contadorB++;
+      } else if (jugadorA == "tijera" && jugadorB == "papel"){
+         contadorA++;
+      } else if (jugadorA == "papel" && jugadorB == "tijera") {
+         contadorB++;
       } else if (jugadorA == "papel" && jugadorB == "piedra") {
-         contadorA = contadorA + 1;
+         contadorA++;
       } else if (jugadorA == jugadorB) {
-         document.body.innerHTML = "Empate";
+         alert("empate");
       } else {
-         document.body.innerHTML = "Valores erróneos"
+         alert("Valores erróneos");
       }
 
-iniciar = prompt("¿Quiere iniciar otro juego? (si/no)");
+iniciar = prompt("¿Quiere iniciar otro juego?" , "si/no");
 }
 
 if (iniciar == "no"){
