00faf631ab8a4af3fe70a5762b15d3f5dd01ed54
Cristian Rivera Acevedo <cristian.rivera3284@gmail.com>
2022-12-27 03:55:59 +0000

    Corrección de contador y jugador victorioso

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-
-let iniciar = prompt ("¿Iniciar?");
+//variables
+let iniciar = prompt ("¿Iniciar el juego? (si/no)");
 let contadorA = 0;
 let contadorB = 0;
 
@@ -11,31 +11,34 @@ while (iniciar == "si")
    let jugadorA = prompt("Jugador A");
    let jugadorB = prompt("Jugador B");
 
-if(jugadorA == "piedra" && jugadorB == "tijeras"){
-  contadorA = contadorA + 1;
- } else if (jugadorA == "piedra" && jugadorB == "papel"){
-   contadorB = contadorB + 1;
- } else if (jugadorA == "tijeras" && jugadorB == "piedra"){
-   contadorB = contadorB + 1;
- } else if (jugadorA == "tijeras" && jugadorB == "papel"){
-   contadorA = contadorA + 1;
- } else if (jugadorA == "papel" && jugadorB == "tijeras") {
-   contadorB = contadorB + 1;
- } else if (jugadorA == "papel" && jugadorB == "piedra") {
-   contadorA = contadorA + 1;
- } else if (jugadorA == jugadorB) {
-    document.body.innerHTML = "Empate";
- } else {
-    document.body.innerHTML = "Valores erróneos"
- }
+      if(jugadorA == "piedra" && jugadorB == "tijeras"){
+      contadorA = contadorA + 1;
+      } else if (jugadorA == "piedra" && jugadorB == "papel"){
+         contadorB = contadorB + 1;
+      } else if (jugadorA == "tijeras" && jugadorB == "piedra"){
+         contadorB = contadorB + 1;
+      } else if (jugadorA == "tijeras" && jugadorB == "papel"){
+         contadorA = contadorA + 1;
+      } else if (jugadorA == "papel" && jugadorB == "tijeras") {
+         contadorB = contadorB + 1;
+      } else if (jugadorA == "papel" && jugadorB == "piedra") {
+         contadorA = contadorA + 1;
+      } else if (jugadorA == jugadorB) {
+         document.body.innerHTML = "Empate";
+      } else {
+         document.body.innerHTML = "Valores erróneos"
+      }
 
- iniciar = prompt("Quiere iniciar otro juego (si/no)");
+iniciar = prompt("¿Quiere iniciar otro juego? (si/no)");
 }
 
 if (iniciar == "no"){
-   document.body.innerHTML = "las victorias del jugador A son: " + contadorA + "<br>las victorias del jugador B son: " + contadorB;
+   
+   if (contadorA > contadorB){
+      document.body.innerHTML = "El ganador es el jugador A con: " + contadorA + " victorias"
+   } else if (contadorA == contadorB){
+      document.body.innerHTML = "Tenemos un empate!"
+   } else {
+      document.body.innerHTML = "El ganador es el jugador B con: " + contadorB + " victorias"
+   }
 }
-
-
- /* hacer contador de número de partidas y victorias de cada jugador
-  hacer el print en html y no en consola document.body.innerHTML */
